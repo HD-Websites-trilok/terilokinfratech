1ec65ef3504ba647edc5d3b544ac9a76890ccb5c
unknown <dhansukhharkhani@gmail.com>
2022-02-25 18:59:10 +0530

    trilok

--- stockmgmt/models.py
+++ stockmgmt/models.py
@@ -30,9 +30,7 @@ class Stock(models.Model):
 	def __str__(self):
 		return self.item_name + " " + str(self.quantity)
 
-
 class StockHistory(models.Model):
-	# category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
 	category = models.CharField(max_length=50, blank=True, null=True, choices=category_choice)	
 	item_name = models.CharField(max_length=50, blank=True, null=True)
 	quantity = models.IntegerField(default='0', blank=True, null=True)
@@ -46,3 +44,5 @@ class StockHistory(models.Model):
 	reorder_level = models.IntegerField(default='0', blank=True, null=True)
 	last_updated = models.DateTimeField(auto_now_add=False, auto_now=False, null=True)
 	timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, null=True)
+
+
